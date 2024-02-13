ab1eb8ec89efce9a37900f2317ef1c5d2832dc76
IsThisTheCrustyCrab-was-taken <kampe@uni-potsdam.de>
2024-02-13 16:12:23 +0100

    Initial Commit

---
+++ WebAppify/WebAppifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebAppifyApp.swift
+//  WebAppify
+//
+//  Created by Bennet Kampe on 13.02.24.
+//
+
+import SwiftUI
+
+@main
+struct WebAppifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
